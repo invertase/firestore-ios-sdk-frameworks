e40724e5370e227a3bceb3c8b76eb1c557e3ffc5
russellwheatley <russellwheatley85@gmail.com>
2024-04-02 15:27:20 +0100

    update constants

--- .github/workflows/scripts/constants.dart
+++ .github/workflows/scripts/constants.dart
@@ -2,19 +2,21 @@ import 'dart:io';
 const bool debugOutput = false;
 final bool isCI = Platform.environment['CI'] == 'true';
 
+const pathToScripts = '.github/workflows/scripts/';
+
 //Scripts to be executed in order
-const String extractVersionsScript = './extract-versions.sh';
+const String extractVersionsScript = 'extract-versions.sh';
 const String firestoreVersionJSONPath = './tmp/firestore_versions.json';
 
-const String extractRawZipUrlsScript = './extract-urls.sh';
+const String extractRawZipUrlsScript = 'extract-urls.sh';
 const String firestoreRawZipUrlsJSONPath = './tmp/firestore_urls.json';
 
 const String extractPrivacyManifestURLSScript =
     './extract-privacy-manifest-urls.sh';
 const String privacyManifestUrlsJSONPath = './tmp/privacy_manifest_urls.json';
 
-const String writeNewZipScript = './create-zips.sh';
+const String writeNewZipScript = 'create-zips.sh';
 
-const String updateFileVariableValues = './update-file-variables.sh';
+const String updateFileVariableValues = 'update-file-variables.sh';
 
-const String commitAndPublishScript = './commit-and-publish.sh';
+const String commitAndPublishScript = 'commit-and-publish.sh';
